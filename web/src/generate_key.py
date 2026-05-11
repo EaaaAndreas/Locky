3ddeb634d26501c1b306bb89d35617c9cba75fd2
Madssb44 <you@example.com>
2026-05-11 15:07:49 +0200

    Working AES-256 encryption, ready to get secret key from .env file

--- web/src/generate_key.py
+++ web/src/generate_key.py
@@ -1,35 +1,36 @@
 from cryptography.hazmat import backends
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from hashlib import sha256 
 from os import urandom, getenv 
 import base64 
 
-
-
-def encrypt_data(key=getenv("SECRET_KEY"), plaintext=None) -> str:
+def encrypt_data(key=sha256(b'{getenv("SECRET_KEY")}').hexdigest(), plaintext=None) -> str:
+    key = sha256(b'{key}').digest()
     iv = urandom(16)
 
-    cipher = Cipher(algorithms.AES(key), modes.CFB(iv), backends=default_backend())
+    cipher = Cipher(algorithms.AES(key), modes.CFB(iv), backend=default_backend())
     encryptor = cipher.encryptor()
 
     ciphertext = encryptor.update(plaintext.encode()) + encryptor.finalize()
 
     return base64.b64encode(iv + ciphertext).decode('utf-8')
 
-def decrypt_data(key=getenv("SECRET_KEY"), encrypted_data=None) -> str:
-   encrypted_data_bytes = base64.b64decode(encrypted_data)
+def decrypt_data(key=sha256(b'{getenv("SECRET_KEY")}').hexdigest(), encrypted_data=None) -> str:
+    key = sha256(b'{key}').digest()
+    encrypted_data_bytes = base64.b64decode(encrypted_data)
 
-   iv = encrypted_data_bytes[:16]
-   ciphertext = encrypted_data_bytes[16:]
+    iv = encrypted_data_bytes[:16]
+    ciphertext = encrypted_data_bytes[16:]
 
-   cipher = Cipher(algorithms.AES(key), modes.CFB(iv), backend=default_backend())
-   decryptor = cipher.decryptor()
+    cipher = Cipher(algorithms.AES(key), modes.CFB(iv), backend=default_backend())
+    decryptor = cipher.decryptor()
 
-   decrypted_data = decryptor.update(ciphertext) + decryptor.finalize()
+    decrypted_data = decryptor.update(ciphertext) + decryptor.finalize()
 
-   return decrypted_data
+    return decrypted_data.decode()
 
-
-test = encrypt_data(key=b"testasdfasdfsadfasdfasdfsadfasdfasdf", plaintext="testing encryption")
+key = sha256(b"superdupersecretkey").hexdigest()
+test = encrypt_data(key=key, plaintext="testing encryption")
 print("Encrypted:", test)
-print("Decrypted:", decrypt_data(key=b"testasdfasdfasdfasdfasdfasdfasdf", encrypt_data=test))
+print("Decrypted:", decrypt_data(key=key, encrypted_data=str(test)))
